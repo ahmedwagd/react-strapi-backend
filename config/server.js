f9adef32035a6b260ca0b688321788a3b0ad5350
ahmedwagd <ahmedwa122@gmail.com>
2021-08-07 07:04:43 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8967c732f43a7be7ce490fed10e3f703'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6c3ba75b-46e2-4750-9f4b-9100b21b9002'
+};
